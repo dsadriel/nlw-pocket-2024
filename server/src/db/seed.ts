1f33c2ea9d3e81193c742f69835cc39710fe87d2
dsadriel <ds.adriel@gmail.com>
2024-09-12 12:41:35 -0300

    Update seed.ts

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -22,22 +22,20 @@ async function seed() {
   const startOfWeek = dayjs().startOf('week')
   await db.insert(goalCompletions).values([
     { goalId: result[0].id, createdAt: startOfWeek.toDate() }, // Acordar cedo - Dia 1
-    { goalId: result[0].id, createdAt: startOfWeek.add(1, 'day').toDate() }, // Acordar cedo - Dia 2
-    { goalId: result[0].id, createdAt: startOfWeek.add(2, 'day').toDate() }, // Acordar cedo - Dia 3
-    { goalId: result[0].id, createdAt: startOfWeek.add(3, 'day').toDate() }, // Acordar cedo - Dia 4
-    { goalId: result[0].id, createdAt: startOfWeek.add(4, 'day').toDate() }, // Acordar cedo - Dia 5
+    { goalId: result[0].id, createdAt: startOfWeek.add(0, 'day').toDate() }, // Acordar cedo - Dia 2
+    { goalId: result[0].id, createdAt: startOfWeek.add(1, 'day').toDate() }, // Acordar cedo - Dia 3
 
     { goalId: result[1].id, createdAt: startOfWeek.toDate() }, // Me exercitar - Dia 1
     { goalId: result[1].id, createdAt: startOfWeek.add(2, 'day').toDate() }, // Me exercitar - Dia 3
-    { goalId: result[1].id, createdAt: startOfWeek.add(4, 'day').toDate() }, // Me exercitar - Dia 5
+    { goalId: result[1].id, createdAt: startOfWeek.add(3, 'day').toDate() }, // Me exercitar - Dia 5
 
-    { goalId: result[2].id, createdAt: startOfWeek.add(1, 'day').toDate() }, // Meditar - Dia 2
+    { goalId: result[2].id, createdAt: startOfWeek.add(2, 'day').toDate() }, // Meditar - Dia 2
     { goalId: result[2].id, createdAt: startOfWeek.add(3, 'day').toDate() }, // Meditar - Dia 4
 
-    { goalId: result[3].id, createdAt: startOfWeek.add(3, 'day').toDate() }, // Ler um livro - Dia 4
-    { goalId: result[4].id, createdAt: startOfWeek.add(4, 'day').toDate() }, // Cozinhar refeições saudáveis - Dia 5
-    { goalId: result[5].id, createdAt: startOfWeek.add(5, 'day').toDate() }, // Escrever no diário - Dia 6
-    { goalId: result[6].id, createdAt: startOfWeek.add(6, 'day').toDate() }, // Aprender algo novo - Dia 7
+    { goalId: result[3].id, createdAt: startOfWeek.add(1, 'day').toDate() }, // Ler um livro - Dia 4
+    { goalId: result[4].id, createdAt: startOfWeek.add(0, 'day').toDate() }, // Cozinhar refeições saudáveis - Dia 5
+    { goalId: result[5].id, createdAt: startOfWeek.add(2, 'day').toDate() }, // Escrever no diário - Dia 6
+    { goalId: result[6].id, createdAt: startOfWeek.add(0, 'day').toDate() }, // Aprender algo novo - Dia 7
   ])
 }
 
